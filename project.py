d358bf1780eec540feab9217bfa77a855ea2c9f6
paoloitaliani <paoita@hotmail.it>
2021-01-30 05:43:01 +0100

    commit

--- project.py
+++ project.py
@@ -6,11 +6,16 @@ import imutils
 import time
 import cv2
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--prototxt", required=True,
-	help="path to Caffe 'deploy' prototxt file")
-ap.add_argument("-m", "--model", required=True,
-	help="path to Caffe pre-trained model")
-ap.add_argument("-c", "--confidence", type=float, default=0.5,
-	help="minimum probability to filter weak detections")
-args = vars(ap.parse_args())
+prototxt="/Users/Niolo/Documents/Github/Image-Tracker/models/deploy.prototxt.txt"
+
+caffemodel="/Users/Niolo/Documents/Github/Image-Tracker/models/res10_300x300_ssd_iter_140000.caffemodel"
+
+ct = CentroidTracker()
+(H, W) = (None, None)
+
+print("[INFO] loading model...")
+net = cv2.dnn.readNetFromCaffe(prototxt, caffemodel)
+# initialize the video stream and allow the camera sensor to warmup
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
